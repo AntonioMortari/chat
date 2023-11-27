f463b4b72470dc6121f9dd99d8e631e23738fe0f
Antonio Mortari <antoniomortari09@gmail.com>
2023-11-27 12:11:24 -0300

    update

--- server/src/server.js
+++ server/src/server.js
@@ -4,7 +4,7 @@ const {Server} = require('socket.io')
 
 const app = express()
 const httpServer = http.createServer(app)
-const io = new Server(httpServer, { cors: { origin: 'http://localhost:5173'}})
+const io = new Server(httpServer, { cors: { origin: 'https://chat-pi-azure.vercel.app/'}})
 let connectedUsers = 0
 
 io.on('connection', socket => {
